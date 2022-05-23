77a4db4b51c1b6075b82bba326ab77ebf69657cf
xtian-2000 <christian.gealone1@gmail.com>
2022-05-24 02:56:51 +0800

    boxly v1.4

--- index.php
+++ index.php
@@ -9,22 +9,35 @@
         <meta name="viewport" content="width=device-width, initial-scale-1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Boxly | Inventory Management System</title>
+        
+        <!-- === CSS=== -->
         <link rel="stylesheet" type="text/css" href="style.css">
+        <!-- === Iconscout CSS=== -->
+        <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+
     </head>
     <body>
-        <header> </header>
-        <div id="login" class="login">
-            <h2>Login</h2><br>
-            <form action="login_action.php" method="POST">
-                <label for="username">Username:</label><br>
-                <input type="text" id="username" name="username" required><br>
-
-                <label for="password">Password:</label><br>
-                <input type="password" id="password" name="password" required><br>
-
-                <input type="submit" value="Submit" name="SubmitButton">
-            </form>
-            <a href="register.php">Register here</a>
+        <div class="container">
+            <div class="form">
+                <div id="login" class="form login">
+                    <span class="title">Login</span>
+                    <form action="login_action.php" method="POST">
+                        <div class="input-field">
+                            <input type="text" placeholder="Username" id="username" name="username" required>
+                            <i class="uil uil-user"></i>
+                        </div>
+                        <div class="input-field">
+                            <input type="password" placeholder="Enter Password" id="password" name="password" required>
+                            <i class="uil uil-lock icon"></i>
+                            <i class="uil uil-eye-slash showHidePw"></i>
+                        </div>
+                        <div class="checkbox-text">
+                            <input type="submit" value="Submit" name="SubmitButton">
+                            <a href="register.php" class="text">Register here</a>
+                        </div>      
+                    </form>   
+                </div>
+            </div>
         </div>
     </body>
 </html>
