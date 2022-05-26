1af4f36925283aee8c6c24cbd70455091521b21a
evguevarra <evguevarra08@gmail.com>
2022-05-26 19:23:05 +0800

    Fixed transaction_list background

--- transaction_list.php
+++ transaction_list.php
@@ -5,42 +5,42 @@
 
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale-1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Boxly | Inventory Management System</title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale-1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Boxly | Inventory Management System</title>
 
-        <!-- === CSS=== -->
-        <link rel="stylesheet" type="text/css" href="style.css">
-        <!-- CSS only -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-            integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- === CSS=== -->
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
-        <script src="table2excel.js"></script>
-    </head>
+    <script src="table2excel.js"></script>
+</head>
 
-    <body>
-        <div class="container my-5">
-            <h1>Transactions</h1>
-            <button class="btn btn-secondary" role="button" id="export_button">Export data</button>
-            <br>
-            <table class="table" id="transaction_data">
-                <thead>
-                    <tr>
-                        <th>Transaction ID</th>
-                        <th>Transaction Category</th>
-                        <th>Product Name</th>
-                        <th>Quantity</th>
-                        <th>Price (per item)</th>
-                        <th>Total value</th>
-                        <th>Date</th>
-                        <th>Time</th>
-                        <th> </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php 
+<body>
+    <div class="container bg-light rounded my-5">
+        <h1>Transactions</h1>
+        <button class="btn btn-secondary" role="button" id="export_button">Export data</button>
+        <br>
+        <table class="table" id="transaction_data">
+            <thead>
+                <tr>
+                    <th>Transaction ID</th>
+                    <th>Transaction Category</th>
+                    <th>Product Name</th>
+                    <th>Quantity</th>
+                    <th>Price (per item)</th>
+                    <th>Total value</th>
+                    <th>Date</th>
+                    <th>Time</th>
+                    <th> </th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
                             $hostName = "ims.cm10enqi961k.us-east-2.rds.amazonaws.com";
                             $userName = "pongodev";
                             $password = "pongodevPongodev";
@@ -75,18 +75,16 @@
                             } 
                         ?>
 
-                </tbody>
-            </table>
-        </div>
-    </body>
-    <script>
+            </tbody>
+        </table>
+    </div>
+</body>
+<script>
+document.getElementById('export_button').addEventListener('click', function() {
+    var table2excel = new Table2Excel();
+    table2excel.export(document.querySelectorAll("#transaction_data"));
 
-        document.getElementById('export_button').addEventListener('click', function(){
-        var table2excel = new Table2Excel();
-        table2excel.export(document.querySelectorAll("#transaction_data"));
-
-    });
-        
-    </script>
+});
+</script>
 
 </html>
